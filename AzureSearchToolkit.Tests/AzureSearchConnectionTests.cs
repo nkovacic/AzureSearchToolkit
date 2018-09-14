79e06b4d04b79edc005e6a07a0ad0d10c8f78796
nkovacic <niko.kovacic2@gmail.com>
2018-09-14 10:56:22 +0200

    Added Tests.

---
+++ AzureSearchToolkit.Tests/AzureSearchConnectionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AzureSearchToolkit.Tests
+{
+    public class AzureSearchConnectionTests
+    {
+        const string SearchName = "searchName";
+        const string SearchKey = "searchKey";
+
+        [Fact]
+        public static void GuardClauses_Constructor()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AzureSearchConnection(null, null));
+            Assert.Throws<ArgumentException>(() => new AzureSearchConnection(null, null, index: ""));
+        }
+
+        [Fact]
+        public void GuardClauses_Timeout()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AzureSearchConnection(SearchName, SearchKey, timeout: TimeSpan.FromDays(-1)));
+        }
+    }
+}
