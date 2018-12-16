43cd1548d60973414574ac2933212db5e0cdf0a7
nkovacic <niko.kovacic2@gmail.com>
2018-12-16 13:03:45 +0100

    Added Index attribute to Listing.

--- AzureSearchToolkit.IntegrationTest/Models/Listing.cs
+++ AzureSearchToolkit.IntegrationTest/Models/Listing.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Search;
+﻿using AzureSearchToolkit.Attributes;
+using Microsoft.Azure.Search;
 using Microsoft.Azure.Search.Models;
 using Microsoft.Spatial;
 using System;
@@ -9,6 +10,7 @@ using System.Text;
 
 namespace AzureSearchToolkit.IntegrationTest.Models
 {
+    [AzureSearchIndex(Data.Index)]
     [SerializePropertyNamesAsCamelCase]
     class Listing : IEquatable<Listing>
     {
